718bbd2d95f4717275035235ce680665314976af
Ethan Luna <ethanlunacado@gmail.com>
2021-10-28 22:34:09 -0500

    I added the date

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ import images from './assets/images';
 import './components/style.css'
 
 function App() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //January is 0!
+  var yyyy = today.getFullYear();
+
   return (
     <div>
       <Navbar />
       <div id='header' className='display-2 text-center'>Hi, Welcome to my Daily Web Page👋</div>
+      <div id='header' className='display-4 text-center'>Today is {dd}/{mm}/{yyyy}.Rise and Shine!</div>
       <Text />
       <Card/>
 
